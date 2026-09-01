6e5bafcc15a58f9928967b606244b83eeb641e18
kunjalp <kunjalp126@gmail.com>
2026-09-01 11:28:01 -0700

    Adjust for conflicts with native setting

--- web-app/capacitor.config.ts
+++ web-app/capacitor.config.ts
@@ -7,6 +7,11 @@ const config: CapacitorConfig = {
   server: {
     url: 'https://ucsc-dining-tracker.vercel.app/',
     cleartext: false
+  },
+  ios: {
+    contentInset: 'never',
+    backgroundColor: '#0b1326',
+    scrollEnabled: true,
   }
 };
 
