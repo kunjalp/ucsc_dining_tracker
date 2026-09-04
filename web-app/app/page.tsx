6d6cf58483a3f803c8c2bd30c6ae560f4ac91339
kunjalp <kunjalp126@gmail.com>
2026-09-04 01:07:26 -0700

    Adjust logo size to midpoint

--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -132,7 +132,7 @@ export default function AuthPage() {
           <img
             src="/sammy-logo-transparent.png"
             alt="Sammy's Palate"
-            className="h-20 w-20 object-contain shrink-0"
+            className="h-16 w-16 object-contain shrink-0"
           />
 
           <div className="h-11 w-px bg-white/15" />
